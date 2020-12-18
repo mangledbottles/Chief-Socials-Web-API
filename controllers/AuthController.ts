44544da07312a06694c1af83fb1ba789b0ca09db
Dermot O'Brien <contact@dermotobrien.me>
2020-12-18 10:42:28 +0000

    Entity: trying different things..

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -1,5 +1,5 @@
-import { connect } from "../config";
 import { Users } from "../entity/UsersEntity";
+import { connect } from "../config";
 
 exports.createUser = ({ name, email, password }) => {
   return new Promise(async (resolve, reject) => {
