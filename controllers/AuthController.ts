08fe42d8053cd5ecd4131ab0fd1b92f3ab3c95c9
Dermot O'Brien <contact@dermotobrien.me>
2020-12-18 10:46:25 +0000

    Entity: trying changes to async

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -1,5 +1,5 @@
-import { Users } from "../entity/UsersEntity";
 import { connect } from "../config";
+import { Users } from "../entity/UsersEntity";
 
 exports.createUser = ({ name, email, password }) => {
   return new Promise(async (resolve, reject) => {
@@ -7,17 +7,19 @@ exports.createUser = ({ name, email, password }) => {
       if (!name || !email || !password)
         reject({ status: 400, message: "Missing parameters" });
 
-      const connection = await connect();
-      const repo = connection.getRepository(Users);
+      // const connection = await connect()
+      connect().then(async (connection) => {
+        const repo = connection.getRepository(Users);
 
-      const newUser = new Users();
-      newUser.name = name;
-      newUser.email = email;
-      newUser.password = password;
+        const newUser = new Users();
+        newUser.name = name;
+        newUser.email = email;
+        newUser.password = password;
 
-      const savedUser = await repo.save(newUser);
+        const savedUser = await repo.save(newUser);
 
-      resolve(savedUser);
+        resolve(savedUser);
+      });
     } catch (error) {
       reject(error);
     }
