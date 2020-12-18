966b80cffd96f01346c0ca4738f94becea02c4d5
Dermot O'Brien <contact@dermotobrien.me>
2020-12-18 10:45:27 +0000

    Routing changes, 404 management and cleanup

--- app.ts
+++ app.ts
@@ -15,23 +15,15 @@ app.use(express.json());
 const AuthRoute = require("./routes/AuthRoute");
 const UserRoute = require("./routes/UserRoute");
 
-app.all("/", (req, res) => {
-  res.json({ message: "CHIEF SOCIALS API " });
-});
-
-app.all("/a/api", (req, res) => {
-  res.json({ message: "Yup its like this" });
-});
-
 app.all("/api", (req, res) => {
   res.json({ message: " /api CHIEF SOCIALS API" });
 });
 app.use("/api/auth", AuthRoute);
 app.use("/api/users", UserRoute);
 
-// app.use((err, req, res, next) => {
-//   res.status(404).json({ message: "Chief Socials API", endpoint: "NOT_FOUND" });
-// });
+app.use((err, req, res, next) => {
+  res.status(404).json({ message: "Chief Socials API", endpoint: "NOT_FOUND" });
+});
 
 app.listen(3000, () => {
   console.log(`Running on http://localhost:3000`);
