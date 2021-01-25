aa1509e4765b85140ffbd417b609458ff32788b2
Dermot O'Brien <contact@dermotobrien.me>
2021-01-25 12:37:00 +0000

    Change port to 8080 for AWS

--- app.ts
+++ app.ts
@@ -27,6 +27,6 @@ app.use((err, req, res, next) => {
   res.status(404).json({ message: "Chief Socials API", endpoint: "NOT_FOUND" });
 });
 
-app.listen(3000, () => {
-  console.log(`Running on http://localhost:3000`);
+app.listen(8080, () => {
+  console.log(`Running on http://localhost:8080`);
 });
